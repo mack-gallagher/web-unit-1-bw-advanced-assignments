6a18a159f334c7a0b28fc2c26b66b90ed7b468b8
Mack Gallagher <wingsonthebus@gmail.com>
2022-04-02 23:16:56 -0500

    Implemented dark theme - light theme switcher; still unsatisfactory bc had to use workaround with global scope

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,1 +1,19 @@
-// you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
+const toggleButton = document.querySelector(".toggle-theme");
+
+const myStyleSheets = document.styleSheets;
+for (let i = 0; i < myStyleSheets.length; i++) {
+  console.log(myStyleSheets[i].ownerNode.id);
+  if (myStyleSheets[i].ownerNode.id === "dark-theme") {
+    var darkStyleSheet = myStyleSheets[i];
+  } else if (myStyleSheets[i].ownerNode.id === "light-theme") {
+    var lightStyleSheet = myStyleSheets[i];
+    lightStyleSheet.disabled = true;
+  }
+}
+
+function toggleTheme(firstSheet,secondSheet) { 
+  firstSheet.disabled = !firstSheet.disabled;
+  secondSheet.disabled = !secondSheet.disabled;
+}
+
+toggleButton.addEventListener("click",event => toggleTheme(darkStyleSheet,lightStyleSheet));
