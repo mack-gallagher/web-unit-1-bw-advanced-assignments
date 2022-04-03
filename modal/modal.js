018505e62772a7067f8aa35f12043674527a068c
Mack Gallagher <wingsonthebus@gmail.com>
2022-04-03 00:05:59 -0500

    implemented very basic modal

--- modal/modal.js
+++ modal/modal.js
@@ -1,1 +1,37 @@
 // you will need to toggle the visibility off and on with DOM selectors
+
+
+/* getting elements */
+
+const modalTrigger = document.querySelector(".modal-trigger");
+
+const modalCloser = document.querySelector(".close-modal");
+
+/* end getting elements */
+
+
+
+/* event listeners */
+
+function triggerModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = 'flex';
+}
+
+function closeModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = 'none';
+}
+
+/* end event listeners */
+
+
+
+
+/* hooking up event listeners */
+
+modalTrigger.addEventListener('click',triggerModal);
+
+modalCloser.addEventListener('click',closeModal);
+
+/* end hooking up event listeners */
